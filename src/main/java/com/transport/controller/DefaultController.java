20c7f95658cb7b61707a647e6df8ef83fa1404bb
Ravinder Kadiyan <rkadiyan@thoughtworks.com>
2018-05-13 15:26:50 +0530

    minor refactoring

--- src/main/java/com/transport/controller/DefaultController.java
+++ src/main/java/com/transport/controller/DefaultController.java
@@ -2,48 +2,49 @@ package com.transport.controller;
 
 import com.transport.model.Inquiry;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
 
     @GetMapping("/")
-    public String home1(Model model) {
-        model.addAttribute("inquiry", new Inquiry());
-        return "/home";
+    public ModelAndView home1() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("inquiry", new Inquiry());
+        model.setViewName("home");
+        return model;
     }
 
     @GetMapping("/home")
-    public String home(Model model) {
-        model.addAttribute("inquiry", new Inquiry());
-        return "/home";
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return "/admin";
+    public ModelAndView home() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("inquiry", new Inquiry());
+        model.setViewName("home");
+        return model;
     }
 
 
-    @GetMapping("/user")
-    public String user() {
-        return "/user";
-    }
 
     @GetMapping("/about")
-    public String about() {
-        return "/about";
+    public ModelAndView about() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("about");
+        return model;
     }
 
     @GetMapping("/thankyou")
-    public String thankyou() {
-        return "/thankyou";
+    public ModelAndView thankyou() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("thankyou");
+        return model;
     }
 
     @GetMapping("/login")
-    public String login() {
-        return "/login";
+    public ModelAndView login() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("login");
+        return model;
     }
 
     @GetMapping("/403")
